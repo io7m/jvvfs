be978af690755ced4185f9483ac6e6d09185057b
Mark Raynsford <code@io7m.com>
2012-09-05 17:44:47 +0000

    Improve unit test coverage

--- src/test/java/com/io7m/jvvfs/FilesystemClasspathTest.java
+++ src/test/java/com/io7m/jvvfs/FilesystemClasspathTest.java
@@ -1,13 +1,16 @@
 package com.io7m.jvvfs;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 
 import com.io7m.jaux.Constraints.ConstraintError;
-import com.io7m.jvvfs.Filesystem;
-import com.io7m.jvvfs.FilesystemError;
-import com.io7m.jvvfs.PathVirtual;
+import com.io7m.jaux.PropertyUtils;
+import com.io7m.jlog.Log;
+import com.io7m.jvvfs.FilesystemError.Code;
 
 /**
  * Specifically test the slightly unsafe
@@ -16,20 +19,17 @@ import com.io7m.jvvfs.PathVirtual;
 
 public class FilesystemClasspathTest
 {
-  // private static final String archive_dir = "test-archives";
-  // private static Filesystem makeFS()
-  // throws IOException,
-  // ConstraintError,
-  // FilesystemError
-  // {
-  // final Log log =
-  // new Log(
-  // PropertyUtils.loadFromFile("io7m-jvvfs.properties"),
-  // "com.io7m.jvvfs",
-  // "main");
-  // return new Filesystem(log, new PathReal(
-  // FilesystemClasspathTest.archive_dir));
-  // }
+  private static Filesystem makeFS()
+    throws IOException,
+      ConstraintError
+  {
+    final Log log =
+      new Log(
+        PropertyUtils.loadFromFile("io7m-jvvfs.properties"),
+        "com.io7m.jvvfs",
+        "main");
+    return new Filesystem(log);
+  }
 
   /**
    * Mount a file that exists inside a directory on the classpath.
@@ -39,42 +39,34 @@ public class FilesystemClasspathTest
    * @throws ConstraintError
    */
 
-  @Test public void testFilesystemClasspathMountFile()
-    throws IOException,
-      FilesystemError,
-      ConstraintError
+  @SuppressWarnings("static-method") @Test public
+    void
+    testFilesystemClasspathMountFile()
+      throws IOException,
+        FilesystemError,
+        ConstraintError
   {
-    // TODO: Work out how to make this test work with maven builds.
-    // final Filesystem fs = FilesystemClasspathTest.makeFS();
-    // fs.mountUnsafeClasspathItem(this.getClass(), new PathVirtual("/"));
-    //
-    // final InputStream stream =
-    // fs.openFile(new PathVirtual(
-    // "/com/io7m/jvvfs/tests/FilesystemClasspathTest.class"));
-    // assert stream != null;
+    final Filesystem fs = FilesystemClasspathTest.makeFS();
+    fs.mountUnsafeClasspathItem(Filesystem.class, new PathVirtual("/"));
+
+    final InputStream stream =
+      fs.openFile(new PathVirtual("/com/io7m/jvvfs/Filesystem.class"));
+    assert stream != null;
   }
 
-  /**
-   * Mount a file that exists inside Example.jar on the classpath.
-   * 
-   * @throws IOException
-   * @throws FilesystemError
-   * @throws ConstraintError
-   */
-
-  @Test public void testFilesystemClasspathMountJar()
-    throws IOException,
-      FilesystemError,
-      ConstraintError
+  @SuppressWarnings("static-method") @Test(expected = FilesystemError.class) public
+    void
+    testFilesystemClasspathMountRejected()
+      throws IOException,
+        ConstraintError,
+        FilesystemError
   {
-    // TODO: Work out how to make this test work with maven builds.
-    // final Filesystem fs = FilesystemClasspathTest.makeFS();
-    // fs.mountUnsafeClasspathItem(
-    // com.io7m.jvvfs_examples.Example.class,
-    // new PathVirtual("/"));
-    //
-    // final InputStream stream =
-    // fs.openFile(new PathVirtual("/com/io7m/jvvfs_examples/Example.class"));
-    // assert stream != null;
+    try {
+      final Filesystem fs = FilesystemClasspathTest.makeFS();
+      fs.mount("nonexistent", "/");
+    } catch (final FilesystemError e) {
+      Assert.assertEquals(Code.FS_ERROR_NONEXISTENT, e.code);
+      throw e;
+    }
   }
 }
